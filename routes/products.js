6985fed831c1a1cdb3acb4d43457c3c3357f1f4c
Harim Muzamil <harim.muzamil@gmail.com>
2022-04-23 16:52:13 +0300

    connecting products api with db

--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,34 @@
 const express = require("express")
 const router = express.Router()
-router.get("/",(req,res)=>{
-    res.json({
-        inCart: 0,
-        tag: "Craft",
-        price: 100,
-        Description: 'fADV charge warm jacket M',
-        images: "Screenshot (11).jpeg"
-    })
+router.get("/", async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = (() => {
+        return new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+    })();
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM public.products ORDER BY product_id ASC;');
+        const results = { 'results': (result) ? result.rows : null };
+        res.json(results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.json({ error: err });
+    }
+
 })
 
-router.post("/new",(req,res)=>{
+router.post("/new", (req, res) => {
     res.json("adding a new product")
 })
 
-router.get("/:id",(req,res) =>{
-    res.json("look for product with id "+req.params.id)
-} )
+router.get("/:id", (req, res) => {
+    res.json("look for product with id " + req.params.id)
+})
 
 module.exports = router

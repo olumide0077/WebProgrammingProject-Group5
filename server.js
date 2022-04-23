bc400156e1c176d64eca728fb34a64a8bab52e7d
Harim Muzamil <harim.muzamil@gmail.com>
2022-04-23 21:21:32 +0300

    adding more routs for rest apis for all the database tables

--- server.js
+++ server.js
@@ -7,7 +7,23 @@ app.get('/',(req,res) => {
     res.json({message:"work in progress"})
 } )
 
+const brandRouter = require("./routes/brands")
+app.use("/brands",brandRouter)
+
+const customerRouter = require("./routes/customers")
+app.use("/customers",customerRouter)
+
+const orderRouter = require("./routes/orders")
+app.use("/orders",orderRouter)
+
+const paymentRouter = require("./routes/payments")
+app.use("/payments",paymentRouter)
+
 const prodRouter = require("./routes/products")
 app.use("/products",prodRouter)
+
+const userRouter = require("./routes/users")
+app.use("/users",userRouter)
+
 
 app.listen(port, () => console.log("Listening on port:" +port))

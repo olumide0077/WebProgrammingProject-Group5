e2417f102c63499b58dc3ff5665a1da83954fbc9
Harim Muzamil <harim.muzamil@gmail.com>
2022-04-24 16:51:53 +0300

    fixing product post

--- routes/products.js
+++ routes/products.js
@@ -42,8 +42,37 @@ router.get("/", async (req, res) => {
     }
 
 })
+/**
+ * @swagger
+ * /products/new:
+ *    post:
+ *      description: Use to add new product
+ *    consumes:
+ *      - application/json
+ *    parameters:
+ *      - name: brand name
+ *        in: body
+ *        description: Name of product
+ *        required: true          
+ *        properties:
+ *          name:
+ *              type: string *        
+ *        schema:
+ *          type: object
+ *          format: application/json
+ *          example: {"name": "longines"}
+ *    responses:
+ *      '200':
+ *        description: Successfully added a brand
+ */
 
 router.post("/new", async (req, res) => {
+    const query = `INSERT INTO products(
+        brand_id, product_name, product_description, size, color, available_product, price, weight, picture, note)
+        VALUES (${req.body.brand_id}, \'${req.body.product_name}\', \'${req.body.product_description}\', \'${req.body.size}\',
+        \'${req.body.color}\', \'${req.body.available_product}\', \'${req.body.price}\', \'${req.body.weight}\',
+        \'${req.body.picture}\', \'${req.body.note}\');`
+        console.log(query)
     const { Pool } = require('pg');
     const pool = (() => {
         if (process.env.NODE_ENV !== 'production') {
@@ -61,10 +90,9 @@ router.post("/new", async (req, res) => {
         }
     })();
     try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM public.products ORDER BY product_id ASC;');
-        const results = { 'results': (result) ? result.rows : null };
-        res.json(results);
+        const client = await pool.connect();        
+        const result = await client.query(query);
+        res.status(200).send("successfuly added brand");
         client.release();
     } catch (err) {
         console.error(err);

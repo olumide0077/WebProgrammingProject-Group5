da8c3eb04782ca7fcb061a8eaebc6b5edded2785
pankaj chakrabarty <t1chpa00@students.oamk.fi>
2022-05-01 10:50:03 +0300

    updated

--- homepage/main.js
+++ homepage/main.js
@@ -1,57 +1,57 @@
 let products = [
   {
-    id: 0,
-    name: "Fashion_Watch_1",
-    tag: "fashion_watch_1",
+    product_id: 0,
+    product_name: "Fashion_Watch_1",
+    product_tag: "fashion_watch_1",
     price: 49,
-    Description: "Lorem ipsam",
+    product_description: "Lorem ipsam",
     imgSrc: "./wristwatchesImage/2.jpeg",
-    inCart: 0
+    product_in_cart: 0
   },
   {
-    id: 1,
-    name: "Fashion_Watch_2",
-    tag: "fashion_watch_2",
+    product_id: 1,
+    product_name: "Fashion_Watch_2",
+    product_tag: "fashion_watch_2",
     price: 149,
-    Description: "Lorem ipsam",
+    product_description: "Lorem ipsam",
     imgSrc: "./wristwatchesImage/3.jpg",
-    inCart: 0
+    product_in_cart: 0
   },
   {
-    id: 2,
-    name: "Fashion_Watch_3",
-    tag: "fashion_watch_3",
+    product_id: 2,
+    product_name: "Fashion_Watch_3",
+    product_tag: "fashion_watch_3",
     price: 79,
-    Description: "Lorem ipsam",
+    product_description: "Lorem ipsam",
     imgSrc: "./wristwatchesImage/4.jpg",
-    inCart: 0
+    product_in_cart: 0
   },
   {
-    id: 3,
-    name: "Fashion_Watch_4",
-    tag: "fashion_watch_4",
+    product_id: 3,
+    product_name: "Fashion_Watch_4",
+    product_tag: "fashion_watch_4",
     price: 249,
-    Description: "Lorem ipsam",
+    product_description: "Lorem ipsam",
     imgSrc: "./wristwatchesImage/5.jpg",
-    inCart: 0
+    product_in_cart: 0
   },
   {
-    id: 4,
-    name: "Fashion_Watch_5",
-    tag: "fashion_watch_5",
+    product_id: 4,
+    product_name: "Fashion_Watch_5",
+    product_tag: "fashion_watch_5",
     price: 179,
-    Description: "Lorem ipsam",
+    product_Description: "Lorem ipsam",
     imgSrc: "./wristwatchesImage/6.jpg",
-    inCart: 0
+    product_in_cart: 0
   },
   {
-    id: 5,
-    name: "Fashion_Watch_6",
-    tag: "fashion_watch_6",
+    product_id: 5,
+    product_name: "Fashion_Watch_6",
+    product_tag: "fashion_watch_6",
     price: 699,
-    Description: "Lorem ipsam",
+    product_description: "Lorem ipsam",
     imgSrc: "./wristwatchesImage/7.jpg",
-    inCart: 0
+    product_in_cart: 0
   }
   
 ];
@@ -64,10 +64,10 @@ function prodcutsDetails() {
             <div class="column">
             <div class="prntcontainer">
             <div class="imgcontainer">
-                        <img src="${product.imgSrc}" alt="${product.name}">
+                        <img src="${product.imgSrc}" alt="${product.product_name}">
                     </div>
                     <div class="content">
-                        <h2>${product.name}</h2>
+                        <h2>${product.product_name}</h2>
                         <h4 class="price">price :  <span><i class="fa-solid fa-euro-sign"></i>${product.price}</span> </h4>
                         <a class="add-cart cart1"><button type="submit">add to cart <i class="fas fa-plus"></i> </button></a>
                         </div>  
@@ -113,7 +113,7 @@ function cartUnits(product, qnSign = ""){
     if (qnSign == "plus"){
         localStorage.setItem('cartUnits', productUnits + 1);
         document.querySelector('.cart span').textContent = productUnits + 1;
-    }else if(qnSign == "minus" && product.inCart != 0){
+    }else if(qnSign == "minus" && product.product_in_cart != 0){
         localStorage.setItem('cartUnits', productUnits - 1);
         document.querySelector('.cart span').textContent = productUnits - 1;
     }
@@ -126,17 +126,17 @@ function setItems(product){
   let cartItems = localStorage.getItem("productsInCart");
   cartItems = JSON.parse(cartItems);
   if(cartItems != null){
-    if(cartItems[product.tag] == undefined){
+    if(cartItems[product.product_tag] == undefined){
       cartItems ={
         ...cartItems,
-        [product.tag]: product
+        [product.product_tag]: product
       }
     }
-    cartItems[product.tag].inCart +=1 ;
+    cartItems[product.product_tag].product_in_cart +=1 ;
   }else{
-    product.inCart = 1;
+    product.product_in_cart = 1;
     cartItems = {
-        [product.tag]: product
+        [product.product_tag]: product
     }
   }
   
@@ -178,6 +178,9 @@ function showCart() {
   
 
   let cartProducts = document.querySelector(".products");
+  let cartsum = document.querySelector(".cart-ontainer");
+  let checkout = document.querySelector(".payment");
+
   let cartTotal = localStorage.getItem("total");
       if(cartItems && cartProducts) {
     
@@ -188,27 +191,37 @@ function showCart() {
       <div class="products">
       <i class="fa-solid fa-xmark"></i>
       <img src="${item.imgSrc}">
-      <span>${item.name}</span>
+      <span>${item.product_name}</span>
       </div>
       <div class="price"> Price: $${item.price}</div>
       <div class="quantity">Quantity:
       <i class="fa-solid fa-plus"></i>
-      <span> ${item.inCart}</span><i class="fa-solid fa-minus"></i>
+      <span> ${item.product_in_cart}</span><i class="fa-solid fa-minus"></i>
       </div>
-      <div class="total">Total: $${item.inCart * item.price}</div>
+      <div class="total">Total: $${item.product_in_cart * item.price}</div>
       </div>
-      `
+      `;
    });
-
+   
     cartProducts.innerHTML += `
     <div class="cartTotalContainer">
     <h4 class="cartTotalTitle">
-        Cart Total &nbsp;
+        Cart Total:
     </h4>
     <h4 class="cartTotal">
     $${cartTotal}
     </h4>
     `;
+
+
+ 
+    cartProducts.innerHTML += `
+    <div class="payment">
+    <h4 class="paymentTitle">
+    <a onclick="alert('Thank You For Shopping With Us.')">
+    Proceed to checkout</a></div>
+    `;
+ 
 
   }
 
@@ -230,7 +243,7 @@ function changeQuantity(event, action){
       presentQuantity = event.target.parentElement.querySelector("span").textContent;
       console.log(cartItems);
       presentProduct = event.target.parentElement.parentElement.querySelector("span").textContent.toLocaleLowerCase().replace(/ /g,'').trim();
-      cartItems[presentProduct].inCart += 1;
+      cartItems[presentProduct].product_in_cart += 1;
       cartUnits(cartItems[presentProduct],"plus");
       localStorage.setItem("total", Number(cartItems[presentProduct].price) + Number(cartTotal));
       localStorage.setItem('productsInCart', JSON.stringify(cartItems));
@@ -239,8 +252,8 @@ function changeQuantity(event, action){
     else if(action == "subtract"){
       presentQuantity = event.target.parentElement.querySelector("span").textContent;
       presentProduct = event.target.parentElement.parentElement.querySelector("span").textContent.toLocaleLowerCase().replace(/ /g,'').trim();
-      if( cartItems[presentProduct].inCart > 1 ) {
-        cartItems[presentProduct].inCart -= 1;
+      if( cartItems[presentProduct].product_in_cart > 1 ) {
+        cartItems[presentProduct].product_in_cart -= 1;
         cartUnits(cartItems[presentProduct], "minus");
         localStorage.setItem("total", Number(cartTotal) - Number(cartItems[presentProduct].price));
         localStorage.setItem("productsInCart", JSON.stringify(cartItems));
@@ -281,8 +294,8 @@ function removeCart(event, action){
     console.log(cartItems);
     console.log(productName);
     //console.log(cartItems[productName]);
-    localStorage.setItem('cartUnits', productUnits - cartItems[productName].inCart);
-    localStorage.setItem('total', cartTotal - ( cartItems[productName].price * cartItems[productName].inCart));
+    localStorage.setItem('cartUnits', productUnits - cartItems[productName].product_in_cart);
+    localStorage.setItem('total', cartTotal - ( cartItems[productName].price * cartItems[productName].product_in_cart));
     
     productUnits = localStorage.getItem('cartUnits');
     document.querySelector('.cart span').textContent = productUnits;

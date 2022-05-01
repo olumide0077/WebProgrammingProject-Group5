cb146c288e68ef9da1a2f5b22f255981d6af0e6c
harim-fatima <89807126+harim-fatima@users.noreply.github.com>
2022-05-01 18:49:47 +0300

    Revert "Delete server.js"

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const port = process.env.PORT || 3000
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const cors = require('cors');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({origin:'*'}));
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: "1.0.0",
+        title: "Webproject Group5 REST API",
+        description: "REST API Information, the base URL to access the api is (rest-api-webproject.herokuapp.com) ",
+        contact: {
+          name: "Harim Muzamil"
+        },
+        servers: ["https://rest-api-webproject.herokuapp.com"]
+      }
+    },
+    
+    apis: ['server.js','routes/*.js']
+  };
+  
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.get('/',(req,res) => {
+    res.json({message:"work in progress"})
+} )
+
+
+const brandRouter = require("./routes/brands")
+app.use("/brands",brandRouter)
+
+const customerRouter = require("./routes/customers")
+app.use("/customers",customerRouter)
+
+const orderRouter = require("./routes/orders")
+app.use("/orders",orderRouter)
+
+const paymentRouter = require("./routes/payments")
+app.use("/payments",paymentRouter)
+
+const prodRouter = require("./routes/products")
+app.use("/products",prodRouter)
+
+const userRouter = require("./routes/users")
+app.use("/users",userRouter)
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}` ))
